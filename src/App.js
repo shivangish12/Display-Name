f164b18d4d361301aaf20b4217d80f40d9d97c1a
Shivangi Sharma <shanussh@gmail.com>
2024-01-30 01:17:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [displayedName, setDisplayedName] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (firstName && lastName) {
+      setDisplayedName(firstName + " " + lastName);
+      setFormError({ firstName: "", lastName: "" });
+    } else {
+      const errors = {
+        firstName: firstName ? "" : "Please fill out this field.",
+        lastName: lastName ? "" : "Please fill out this field.",
+      };
+      setFormError(errors);
+    }
+  };
+  const isSubmitDisabled = !firstName || !lastName;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {formError.firstName && (
+          <span className="error-tooltip">{formError.firstName}</span>
+        )}
+        <div>
+          <label>Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          {formError.lastName && (
+            <span className="error-tooltip">{formError.lastName}</span>
+          )}
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+
+      {displayedName && <p>Full Name: {displayedName}</p>}
     </div>
   );
 }
